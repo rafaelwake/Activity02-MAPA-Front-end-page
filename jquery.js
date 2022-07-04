20f910e4e7dc96d3af619e0379b96bcf83d37468
rafaelwake <rafaelwake07@gmail.com>
2022-07-04 15:08:58 -0300

    finished - MAPA - SI - PROGRAMAÇÃO FRONT

--- jquery.js
+++
@@ -1,9 +0,0 @@
-document.querySelector(".botao-cadastro").onclick = function () {
-  console.log("clicado em cadastro");
-};
-
-document.querySelector(".acessar-botao").onclick = function () {
-  console.log("OK, Login efetuado");
-
-  document.getElementsByTagName("main").textContent = "";
-};
